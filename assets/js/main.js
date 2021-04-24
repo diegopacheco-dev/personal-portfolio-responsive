beadf2b860fb00bebb76086652f61b3d3d623ab7
ericksonder <ericksonder017@gmail.com>
2021-04-23 20:11:05 -0500

    portfolio terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,11 +74,49 @@ const mixer =  mixitup('.portfolio__container', {
 
 
 /* Link active portfolio */ 
+//html collection con todos los portfolio__item
+const linkPortfolio = document.querySelectorAll('.portfolio__item');
+
+function activePortfolio(e){
+    if(linkPortfolio){
+        //al hacer click en un portfolio__item borramos de los demas la clase active__portfolio
+        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'));
+        //agregamos al elemento activador del evento la clase active-portfolio
+        e.target.classList.add('active-portfolio')  //tambien se puede usar this en vez de e.target
+        console.log("Este es el this de la funcion activePortfolio");
+        console.log(this);
+    }
+}
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
+
 
 
 /*===== SWIPER CAROUSEL =====*/ 
+const swiper = new Swiper('.testimonial__container', {
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
 
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+        640:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView: 3,
+        }
+
+    }
+  }); 
 
 /*===== GSAP ANIMATION =====*/ 
+gsap.from('.home__img', {opacity: 0, duration: 2, delay:.5, x: 60});
+gsap.from('.home__data', {opacity: 0, duration: 2, delay:.8, y: 25});
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay:1, y: 25, ease:'expo.out', stagger:.2});
 
-
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1.5, delay:1, y: 25, ease:'expo.out', stagger:.2});
+gsap.from('.nav__item', {opacity: 0, duration: 1.8, delay:1, y: 25, ease:'expo.out', stagger:.2});
+gsap.from('.home__social-icon', {opacity: 0, duration: 1.8, delay:2.3, y: 25, ease:'expo.out', stagger:.2});
